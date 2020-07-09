514c99474e2abfead0bb546033f3e11b8e4a2fe7
irina <irisha.karpova@gmail.com>
2020-07-09 16:12:51 -0500

    add propTypes

--- src/components/tableDataSet.js
+++ src/components/tableDataSet.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {sortingData} from '../ac'
 import {sortedDataTableSelector} from '../selectors'
 import './table_ui.css'
+
+TableData.propTypes = {
+  dataset: PropTypes.arrayOf(PropTypes.exact({
+    id: PropTypes.string,
+    Name: PropTypes.string,
+    Mass: PropTypes.number,
+    Family: PropTypes.string,
+    Classification: PropTypes.string,
+    Status: PropTypes.string,
+    Binomen: PropTypes.string
+  })).isRequired,
+}
 
 function TableData(props){
 
@@ -32,14 +45,14 @@ function TableData(props){
   })
 
   const tableCells = props.dataset.map((data, index)=>{
-      const {id, Name, Mass, Family, Classification, Status  } = data
+      const {id, Name, Mass, Family, Classification, Status, Binomen  } = data
       return(
         <tr key={id}>
           <td className="index-column">{index}</td>
           <td className="name-column">{Name}</td>
           <td className="mass-column">{Mass}oz</td>
           <td>{Family}</td>
-          <td>{data['Scientific Name']}</td>
+          <td>{Binomen}</td>
           <td>{Classification}</td>
           <td>{Status}</td>
         </tr>
