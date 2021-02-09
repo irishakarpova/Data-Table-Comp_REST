53f434217f3153a4583546886dcf900c2219813d
irina <irisha.karpova@gmail.com>
2021-02-09 09:06:37 -0600

    update provider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import TableData from './components/tableDataSet'
 import Header from './components/header'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import store from './store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <div className='container-fluid p-0'>
-      <Header/>
-      <TableData/>
-    </div>
+    <Provider store={store}>
+      <div className='container-fluid p-0'>
+        <Header/>
+        <TableData/>
+      </div>
+    </Provider>
   )
 }
 
