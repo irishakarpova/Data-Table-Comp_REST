2adbe1894a0c7e2df5c69342391aca5532fa26cb
irina <irisha.karpova@gmail.com>
2021-02-09 11:55:52 -0600

    update

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import { Provider } from 'react-redux'
 function App() {
   return (
     <Provider store={store}>
-      <Header/>
-      <TableData/>
+      <div className='container-fluid p-0'>
+        <Header/>
+        <TableData/>
+      </div>
     </Provider>
   )
 }
